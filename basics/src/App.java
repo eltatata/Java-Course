50de8603cff1a980cca7185925d1e073c9d25015
eltatataa <davidtowo75@gmail.com>
2024-08-04 20:55:44 -0500

    variables

--- basics/src/App.java
+++ basics/src/App.java
@@ -2,5 +2,18 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Hello world
         System.out.println("Hello, World!");
+
+        // ---
+
+        // Variables
+        int number = 10;
+        double decimal = 3.14;
+        boolean flag = true;
+        char character = 'A';
+
+        System.out.println("Number: " + number);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Flag: " + flag);
+        System.out.println("Character: " + character);
     }
 }
