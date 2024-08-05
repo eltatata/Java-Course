f3943322d866f91e9acc8133366281e484386ade
eltatataa <davidtowo75@gmail.com>
2024-08-04 20:58:01 -0500

    string

--- basics/src/App.java
+++ basics/src/App.java
@@ -15,5 +15,38 @@ public class App {
         System.out.println("Decimal: " + decimal);
         System.out.println("Flag: " + flag);
         System.out.println("Character: " + character);
+
+        // ---
+
+        // String (text)
+        String text = "Hello, World!";
+        System.out.println("Text: " + text);
+
+        int length = text.length();
+        System.out.println("Length: " + length);
+
+        char firstCharacter = text.charAt(0);
+        System.out.println("First character: " + firstCharacter);
+
+        String subString = text.substring(0, 5);
+        System.out.println("SubString: " + subString);
+
+        String lowerString = text.toLowerCase();
+        System.out.println("Lower String: " + lowerString);
+
+        String upperString = text.toUpperCase();
+        System.out.println("Upper String: " + upperString);
+
+        int index = text.indexOf("World");
+        System.out.println("Index: " + index);
+
+        String replace = text.replace("World", "Java");
+        System.out.println("Replace: " + replace);
+
+        boolean contains = text.contains("World");
+        System.out.println("Contains: " + contains);
+
+        String trim = "   Hello, World!   ";
+        System.out.println("Trim: " + trim.trim());
     }
 }
